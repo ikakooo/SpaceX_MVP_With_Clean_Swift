703af1f79cf43cb5ed7c34d4883b3ba53baf3624
ikakooo <irchkhitunidze@tbcbank.com.ge>
2022-10-02 22:22:31 +0400

    untraced file add

---
+++ SpaceX/Scenes/PopupLinks/PopupLinksRouter.swift
@@ -0,0 +1,26 @@
+//
+//  PopupLinksRouter.swift
+//  SpaceX
+//
+//  Created by Irakli Chkhitunidzde on 02.10.22.
+//
+
+import UIKit
+
+protocol PopupLinksRouter {
+    func openLink(with link: String?)
+}
+
+class PopupLinksRouterImpl: PopupLinksRouter {
+    
+    private weak var controller: PopupLinksViewController?
+    
+    init(_ controller: PopupLinksViewController?) {
+        self.controller = controller
+    }
+    
+    func openLink(with link: String?) {
+        
+    }
+    
+}
